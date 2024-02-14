1dff3c1c39297dbf025fc51abdde1185ac566b0e
Gaddam013 <159575600+Gaddam013@users.noreply.github.com>
2024-02-14 13:33:50 -0500

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,11 @@ def newDeploy(jobname,ip,appname)
 {
   sh "sudo scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
 }
+def newTest(testfile)
+{
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${testfile}.jar"
+}
+def newDelivery(jobname,ip,appname)
+{
+  sh "sudo scp /home/ubuntu/.jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
